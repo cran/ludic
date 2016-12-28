d7610f3524243da9fe78497199cc4597f599a683
Boris Hejblum <bhejblum@hsph.harvard.edu>
2016-12-28 18:25:05 +0000

    version 0.1.3

---
+++ R/ludic-package.R
@@ -0,0 +1,27 @@
+#' ludic
+#' 
+#' Linkage Using Diagnosis Codes
+#' 
+#' This package implements probabilistic record linkage methods that relies on the use of diagnosis 
+#' codes only, in the absence of direct identifiers .
+#' 
+#' \tabular{ll}{
+#' Package: \tab ludic\cr
+#' Type: \tab Package\cr
+#' Version: \tab ludic 0.1.3\cr
+#' Date: \tab 2016-12-28\cr
+#' License: \tab \href{https://cran.r-project.org/web/licenses/MIT}{The "MIT License" (MIT)}\cr
+#' }
+#' The main function of \code{ludic} is \link{recordLink}.
+#' 
+#' @author Boris P. Hejblum, Tianxi Cai
+#' --- Maintainer: Boris P. Hejblum
+#' 
+#' @docType package
+#' @name ludic-package
+#' @aliases ludic
+#' 
+#' @useDynLib ludic
+#' @importFrom Rcpp sourceCpp
+#' 
+NULL
